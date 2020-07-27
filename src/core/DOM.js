c109f9364f9e756c877821e8e99ec873a325dd3a
puss1juic3 <puss1juic3@gmail.com>
2020-07-27 20:01:07 +0300

    Fixed chain for some methods

--- src/core/DOM.js
+++ src/core/DOM.js
@@ -70,11 +70,13 @@ class DOM {
   }
 
   addClass(className) {
-    return this.$el.classList.add(className);
+    this.$el.classList.add(className);
+    return this;
   }
 
   removeClass(className) {
-    return this.$el.classList.remove(className);
+    this.$el.classList.remove(className);
+    return this;
   }
 
   getId(parse) {
